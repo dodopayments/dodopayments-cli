94e06bf6ea97d64a220e774539619051e211d310
Sancho Godinho <sancho@sg-app.com>
2026-05-10 22:33:46 +0530

    Add exit command to list

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -24,5 +24,6 @@ export const COMMANDS = [
   { command: '/logout',           description: 'Logout from stored accounts' },
   { command: '/update',           description: 'Update the CLI to the latest version' },
   { command: '/clear',            description: 'Clear message history' },
+  { command: '/exit',             description: 'Exit the CLI' },
   { command: '/help',             description: 'Show all commands' },
 ];
